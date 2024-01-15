79caed23b00689517d07712800e7ef8558fd58e7
William-silva-Developer <william958silva@gmail.com>
2024-01-15 15:31:55 -0300

    Imprimindo a quantidade de elementos no array

---
+++ ContandoElementos.py
@@ -0,0 +1,32 @@
+# Copie o array apresentado embaixo no seu editor de código, e imprima no terminal: 
+#a quantidade de elementos que ele possui, o dado salvo no índice 2, 
+#o dado salvo no índice 9, e dado salvo no índice 14.
+
+lista_musicos = [ 
+'Djavan', 
+'Roberto Carlos', 
+'Elis Regina', 
+'Tom Jobim', 
+'Milton Nascimento', 
+'Chico Buarque', 
+'Nara Leão', 
+'Pitty', 
+'Simonal', 
+'Moacir Santos', 
+'Caetano Veloso', 
+'Elza Soares', 
+'Paulinho da Viola', 
+'Yamandú Costa', 'Gal Costa'
+] 
+
+# Imprimindo a quantidade de elementos na lista
+print(f"Quantidade de elementos na lista: {len(lista_musicos)}")
+
+# Imprimindo o dado no índice 2
+print(f"Dado no índice 2: {lista_musicos[2]}")
+
+# Imprimindo o dado no índice 9
+print(f"Dado no índice 9: {lista_musicos[9]}")
+
+# Imprimindo o dado no índice 14
+print(f"Dado no índice 14: {lista_musicos[14]}")
